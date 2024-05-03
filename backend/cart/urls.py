d762cbc253c67eb6140ee172e098ec99cec132e6
Perniciess <74770654+Perniciess@users.noreply.github.com>
2024-05-03 21:46:35 +0300

    Delete backend directory

--- backend/cart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AddToCartView, CartView, RemoveFromCartView
-
-urlpatterns = [
-    path('cart/add/<int:product_id>/', AddToCartView.as_view()),
-	path('cart/user/', CartView.as_view()),
-    path('cart/<int:product_id>/delete/', RemoveFromCartView.as_view()),
-]
